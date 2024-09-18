ec8a02d839da61bd45189bfbe6136fb02c3894ad
mws75 <mwspencer75@gmail.com>
2024-09-17 19:42:32 -0700

    added meta data

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,8 +9,8 @@ import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 
-}
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
